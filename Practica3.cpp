d7f55a08b3432ac40556843680a696a7c918e789
Paul Torres <129992331+MaxMz3943@users.noreply.github.com>
2026-03-27 17:56:45 -0600

    Practica 3
    Entrada y Salida de datos.

---
+++ Practica3.cpp
@@ -0,0 +1,15 @@
+//Maximiliano Torres | 27/03/2026 | Practica 3
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    cout<<"Digite un numero"<<endl;
+    cin>>numero;
+    cout<<"El numero que digito es: "<<numero<<endl;;
+    
+    return 0;
+}
