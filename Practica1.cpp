d14ed9359294f6351c70206c624fb08d4a1c5701
Paul Torres <paulmaximilianot@gmail.com>
2026-03-27 17:07:30 -0600

    Practica1
    Mi primer programa en C++ :)!

---
+++ Practica1.cpp
@@ -0,0 +1,10 @@
+//Maximiliano Torres | 27/03/2026 | Practica 1
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hola genteeee"<<endl;
+    cout<<"Cómo estan?"<<endl;
+    return 0;
+}
