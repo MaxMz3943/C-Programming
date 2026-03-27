4e6072bbeac7f6c3d2beedb6f4114557476fe474
Paul Torres <paulmaximilianot@gmail.com>
2026-03-27 17:27:14 -0600

    Create Practica2.cpp

---
+++ Practica2.cpp
@@ -0,0 +1,18 @@
+//Maximiliano Torres | 27/03/2026 | Practica 2
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numero = 15;
+    float decimal = 4.6;
+    double decimal_mayor = 293.53348;
+    char letra = 'B';
+
+    cout<<numero<<endl;
+    cout<<decimal<<endl;
+    cout<<decimal_mayor<<endl;
+    cout<<letra<<endl;
+
+    return 0;
+}
