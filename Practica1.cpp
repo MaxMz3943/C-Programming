e6da97c9f07de82f22980b5a185df2c826d8a98c
Paul Torres <paulmaximilianot@gmail.com>
2026-05-14 22:12:44 -0600

    refactor: update practices 1 and 2 code and descriptions.

--- Practica1.cpp
+++ Practica1.cpp
@@ -1,10 +1,24 @@
-//Maximiliano Torres | 27/03/2026 | Practica 1
+/* Maximiliano Torres | 27/03/2026 | Practica 1
+
+Objetivo: Esta primera practica realizamos la impresion de texto directamente 
+puesto en el programa gracias a la función cout. Imprimiendo
+mensajes simples como lo son "Hola mundoo!"
+
+Sintaxis
+- cout<<"Mensaje"; = Imprime un mensaje en la terminal.
+- endl<<; = Hace un salto de renglon para que el texto no termine pegado entre si. 
+*/
+
 #include <iostream>
 
 using namespace std;
 
+//Función principal
 int main(){
-    cout<<"Hola genteeee"<<endl;
+
+    //Impresion de datos (Salida)
+    cout<<"Hola mundoo!"<<endl;
     cout<<"Cómo estan?"<<endl;
+
     return 0;
-}
+}   
--- Practica2.cpp
+++ Practica2.cpp
@@ -1,14 +1,28 @@
-//Maximiliano Torres | 27/03/2026 | Practica 2
+/* Maximiliano Torres | 27/03/2026 | Practica 2
+Objetivo: En esta segunda practica tenemos que definir distintos tipos de variables
+como los son enteros, flotantes, double entre otras... Y seguido de esto darles
+un valor para que puedan ser impresas en pantalla.
+
+Sintaxis
+- int nombre_variable = Define una variable de numero entero como lo son 5,3,89.
+- float nombre_variable = Define una variable de un numerod decimal como lo son 3.14, 24.5.
+- double nombre_variable = Al igual que int, se usan numeros enteros pero con la diferencia 
+de que puede manejar numeros muchisimos mas grandes teniendo mas capacidad.
+-char nombre_variable = Define una variable de un solo caracter, por ejemplo A,S,@,;, etc...
+*/
 #include <iostream>
 
 using namespace std;
 
+//Función principal
 int main(){
+    //Variables
     int numero = 15;
     float decimal = 4.6;
     double decimal_mayor = 293.53348;
     char letra = 'B';
 
+    //Impresion de datos (Salida)
     cout<<numero<<endl;
     cout<<decimal<<endl;
     cout<<decimal_mayor<<endl;
